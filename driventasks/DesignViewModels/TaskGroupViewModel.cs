e0ff013eab7d92503b33b914f6c8115ca71f6f6b
Chuma Nnaji <cnnaji2003@gmail.com>
2014-02-08 16:05:56 -0800

    Adding Styling.

--- driventasks/DesignViewModels/TaskGroupViewModel.cs
+++ driventasks/DesignViewModels/TaskGroupViewModel.cs
@@ -7,20 +7,22 @@ namespace driventasks.DesignViewModels
     {
         public TaskGroupViewModel()
         {
-            PrimaryTaskItemViewModel = new TaskItemViewModel("new titile primary ", new Random().Next(0, 1) == 0 ? "" : "new primary description item longer description text yeaaa buddy");
+            PrimaryTaskItemViewModel = new TaskItemViewModel("new titile primary " + new Random().Next(0, 100), "new primary description item longer description text yeaaa buddy");
             TaskItemViewModels = new ObservableCollection<TaskItemViewModel>();
+            IsExpanded = false;
             LoadData();
         }
 
+        public bool IsExpanded { get; set; }
         public TaskItemViewModel PrimaryTaskItemViewModel { get; set; }
         public ObservableCollection<TaskItemViewModel> TaskItemViewModels { get; set; }
 
         public void LoadData()
         {
-            int count = new Random().Next(0, 5);
+            int count = 0;// new Random().Next(0, 5);
             for (int i = 0; i < count; i++)
             {
-                TaskItemViewModels.Add(new TaskItemViewModel("new titile item " + i, new Random().Next(0, 1) == 0 ? "" : "new description item " + i + "longer description text yeaaa buddy"));
+                TaskItemViewModels.Add(new TaskItemViewModel("new titile item " + i, new Random().Next(0, 2) == 1 ? "" : "new description item " + i + "longer description text yeaaa buddy"));
             }
         }
     }
